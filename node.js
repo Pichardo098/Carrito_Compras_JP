5c396e36fde00463db5b4b0ece3a41e02e603c3e
Pichardo098 <jesus098856@gmail.com>
2023-05-03 21:32:29 -0600

    Avance en Estilos y JS

--- node.js
+++ node.js
@@ -1,27 +1,42 @@
+//Manipulación Usuario Ventana
+
+const user_ventana = document.getElementById("registro_usuario")
+
+console.log(user_ventana.style )
+const ocultar_registro = () => {
+    user_ventana.style.display = "none"
+}
+
+const mostrar_registro = () => {
+    user_ventana.style.display = "flex"
+}
+
+const inicio_crear_cuenta = () => {
+    alert ("Lo sentimos, en este momento no es posible realizar la tarea.")
+}
+
 //Manipulación de Card Footer
-let cards = document.getElementsByClassName("info_footer")
+const cards = document.getElementsByClassName("info_footer")
 
 
 const mostrar = (num_btn) => {
-    for(let i = 0; i <= cards.length ; i++){
-        if(cards[i].children[0] === num_btn ){
-            cards[i].children[2].style.display = "block"
-            cards[i].children[1].style.display = "block"
-            cards[i].children[0].style.display = "none"
-        } else {
-            cards[i].children[2].style.display = "none"
-            cards[i].children[1].style.display = "none"
-            cards[i].children[0].style.display = "block"
-        }
+    if(cards[0].children[0] === num_btn ){
+        cards[0].children[2].style.display = "block"
+        cards[0].children[1].style.display = "block"
+        cards[0].children[0].style.display = "none"
+    } else {
+        cards[0].children[2].style.display = "none"
+        cards[0].children[1].style.display = "none"
+        cards[0].children[0].style.display = "block"
     }
+    
 }
 
 const ocultar = (num_btn) => {
-    for(let i = 0; i <= cards.length ; i++){
-        if(cards[i].children[1] === num_btn ){
-            cards[i].children[2].style.display = "none"
-            cards[i].children[0].style.display = "block"
-            cards[i].children[1].style.display = "none"
+        if(cards[0].children[1] === num_btn ){
+            cards[0].children[2].style.display = "none"
+            cards[0].children[0].style.display = "block"
+            cards[0].children[1].style.display = "none"
         }
-    }
+    
 }
