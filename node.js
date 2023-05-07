6ee710f3b40dd9ae1bb20e9bc19e90be43dd762b
Pichardo098 <jesus098856@gmail.com>
2023-05-06 18:03:06 -0600

    Corección de logica btn y MediaQrys Listos

--- node.js
+++ node.js
@@ -18,45 +18,69 @@ $('button').on('click', function(){
 //Manipulación Usuario Ventana
 
 const user_ventana = document.getElementById("registro_usuario")
-
-
-const ocultar_registro = () => {
+const mostrar_registro = document.getElementById("mostrar_registro")
+mostrar_registro.addEventListener("click", () => {
+    if(user_ventana.style.display === "flex"){
+        user_ventana.style.display = ""
+        
+    }else{
+        user_ventana.style.display = "flex"
+        cart[0].style.right = ""
+    }
+})
+const cerrar_registro = document.getElementById("cerrar_usuario")
+cerrar_registro.addEventListener("click" ,() => {
     user_ventana.style.display = "none"
+})
+
+const btn_crear_cuenta = document.getElementsByClassName("botones_user")
+const sesion_crear_cuenta = () => {
+    for(btn of btn_crear_cuenta) {
+        btn.addEventListener("click", ()=>(
+            alert ("Lo sentimos, en este momento no es posible realizar la tarea.")
+        ))
+    }
 }
 
-const mostrar_registro = () => {
-    user_ventana.style.display = "flex"
-}
+sesion_crear_cuenta()
 
-const inicio_crear_cuenta = () => {
-    alert ("Lo sentimos, en este momento no es posible realizar la tarea.")
-}
+//Manipulación de BTN Carrito
+const cart_nav = document.getElementById("cart_nav")
+const cart = document.getElementsByClassName("cart")
+cart_nav.addEventListener("click", (event)=>{
+    let value_cart = cart[0].style.right
+    if(value_cart === ""){
+        cart[0].style.right = 0
+    }else{
+        cart[0].style.right = ""
+    }
+})
+const btn_close_cart = document.getElementsByClassName("btn__close")[0]
+btn_close_cart.addEventListener("click", (event)=>{
+    cart[0].style.right = ""
+})
+
+
 
 //Manipulación de Card Footer
 const cards = document.getElementsByClassName("info_footer")
+const mostrar = document.getElementById("mostrar_1");
 
-
-const mostrar = (num_btn) => {
-    if(cards[0].children[0] === num_btn ){
+mostrar.addEventListener("click", (event)=> {
+    if(cards[0].children[0].id === event.target.dataset.id ){
         cards[0].children[2].style.display = "block"
         cards[0].children[1].style.display = "block"
         cards[0].children[0].style.display = "none"
-    } else {
-        cards[0].children[2].style.display = "none"
-        cards[0].children[1].style.display = "none"
-        cards[0].children[0].style.display = "block"
-    }
-    
-}
-
-const ocultar = (num_btn) => {
-        if(cards[0].children[1] === num_btn ){
+    } 
+})
+const ocultar = document.getElementById("ocultar_1")
+ocultar.addEventListener("click", (event)=> {
+    if(cards[0].children[1].id === event.target.dataset.id ){
             cards[0].children[2].style.display = "none"
             cards[0].children[0].style.display = "block"
             cards[0].children[1].style.display = "none"
         }
-    
-}
+})
 
 
 
